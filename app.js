588fa01d014df2454dadbe8fd91505094e87535e
Aguiar <joaomatheusaguiar@gmail.com>
2017-12-20 18:30:59 -0300

    address dynamic

--- app.js
+++ app.js
@@ -3,8 +3,8 @@
 var app = require('./config/server');
 var socketIO = require('socket.io');
 /*Parametrizar a porta de escuta */
-
-var server = app.listen(3000, function(){
+var porta = process.env.PORT || 8080;
+var server = app.listen(porta, function(){
     console.log('Servidor esta online !');
 });
 
