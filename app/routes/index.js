4dbc125a682c3c96cc7c6edc2afd9883f6bca0f3
matheusaguiardev <joaomatheusaguiar@gmail.com>
2017-12-23 21:28:23 -0300

    iniciando view home log

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,6 @@
 module.exports = function(app){
     app.get('/', function(req, res){
+        console.log('Iniciando a view da home !');
         app.app.controllers.index.home(app, req, res);
     });
 }
